8f065184bbef42e376b224faf6daf9789698ecfa
AndresMadeDev <andres.made.dev@gmail.com>
2024-04-18 18:54:52 -0400

    Initial Commit

---
+++ Github-Deom/Github_DeomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_DeomApp.swift
+//  Github-Deom
+//
+//  Created by Andres Made on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_DeomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
